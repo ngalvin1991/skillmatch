1bc34c266c82520f54b851b1bb95013a0b4131aa
MBZAIG <muzzab@amazon.co.uk>
2023-03-15 20:09:36 +0000

    created keys.js for storing API info and added this keys.js to gitignore

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const apiID=APIId;
+const apiKey=APIkeyDat;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
