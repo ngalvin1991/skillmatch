be46a238556965e6d20cb9f410763a7c9c948544
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-03-16 21:01:16 +0000

    new files and code amended

---
+++ src/Components/JobForm/JobForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+
+function JobForm() {
+    return (
+      <div className="form">
+        <h1>Search Jobs</h1>
+        <h4>Enter up to 5 skills, separated by a comma</h4>
+        <h4>Enter your preferred location</h4>
+        <Formik
+          initialValues={{ skills: "Skills", city: "Location" }}
+          onSubmit={async (values) => {
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
+        >
+  
+          
+          <Form>
+            <Field name="skills" type="text" />
+            <Field name="city" type="text" />
+            <button type="submit">Show me my dream job</button>
+          </Form>
+        </Formik>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<JobForm />, document.getElementById("root"));
+
+  export default JobForm;
