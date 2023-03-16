c0c90a24fe70e915c561675139d6767d67ef4594
natalie navickas <natalienavickas@hotmail.com>
2023-03-16 19:20:20 +0000

    form build in footer.js ready to be moved

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,31 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { Formik, Field, Form } from "formik";
+import "./styles.css";
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Search Jobs</h1>
+      <h4>Enter up to 5 skills, separated by a comma</h4>
+      <h4>Enter your preferred location</h4>
+      <Formik
+        initialValues={{ skills: "Skills", city: "Location" }}
+        onSubmit={async (values) => {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <Field name="skills" type="text" />
+          <Field name="city" type="text" />
+          <button type="submit">Show me my dream job</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default Footer; 
