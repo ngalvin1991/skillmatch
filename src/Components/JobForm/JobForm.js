dfb971581b43808f2e3bce0e70e460ff09314111
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-03-17 18:14:24 +0000

    i don't know what happened

--- src/Components/JobForm/JobForm.js
+++ src/Components/JobForm/JobForm.js
@@ -1,60 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Formik, Field, Form } from "formik";
-import { ChakraProvider, Input, Button, FormControl } from "@chakra-ui/react";
 
 function JobForm() {
-  
-  return (
-    <ChakraProvider>
+    return (
       <div className="form">
         <h1>Search Jobs</h1>
         <h4>Enter up to 5 skills, separated by a comma</h4>
         <h4>Enter your preferred location</h4>
         <Formik
-          initialValues={{
-            skills: "",
-            city: ""
-          }}
+          initialValues={{ skills: "Skills", city: "Location" }}
           onSubmit={async (values) => {
             await new Promise((resolve) => setTimeout(resolve, 500));
             alert(JSON.stringify(values, null, 2));
           }}
         >
-          {({ handleSubmit}) => (
-            <Form onSubmit={handleSubmit}>
-              <FormControl>
-              <Field as={Input}
-                id="skills"
-                name="skills"
-                type="text"
-                variant="filled"
-                placeholder="Skills" 
-                _placeholder={{ opacity: 1, color: 'purple.500' }}/>
-                </FormControl>
-                <FormControl>
-              <Field as={Input}
-                id="city"
-                name="city"
-                type="text"
-                variant="outline" 
-                focusBorderColor='purple'
-                placeholder="City"
-                _placeholder={{ opacity: 1, color: 'purple.500' }}/>
-                </FormControl>
-              <Button type="submit" colorScheme="purple" >Show me my dream job</Button>
-            </Form>
-          )}
-         
+  
+          
+          <Form>
+            <Field name="skills" type="text" />
+            <Field name="city" type="text" />
+            <button type="submit">Show me my dream job</button>
+          </Form>
         </Formik>
       </div>
-    </ChakraProvider>
-  );
-}
+    );
+  }
+  
+  ReactDOM.render(<JobForm />, document.getElementById("root"));
 
-ReactDOM.render(<JobForm />, document.getElementById("root"));
-
-export default JobForm;
-//do we want to add validation
-//shall I delete the initial value and replace with empty string and have the placeholder as "skills", "location" instead
-// check how to add isLoading loadingText="Submitting" on click
+  export default JobForm;
