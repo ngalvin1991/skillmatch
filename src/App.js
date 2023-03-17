1f44a8f3dd291d15147b722a631c49ee3d8851d1
natalie navickas <natalienavickas@hotmail.com>
2023-03-17 18:37:52 +0000

    file structure change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
-import Chakra from '@chakra-ui/react';
-import './Components/Contact/Contact';
-import './Components/JobSearch/JobSearch';
-import './Components/JobForm/JobForm'
-import './Components/Jumbotron/Jumbotron';
-import './Components/Navbar/Navbar.js';
-import JobForm from "./Components/JobForm/JobForm";
-import { Navbar } from "react-bootstrap";
+// import Chakra from '@chakra-ui/react';
+// import './Components/Contact/Contact.js';
+// import './Components/JobSearch/JobSearch.js';
+// import './Components/JobForm/JobForm.js'
+// import './Components/Jumbotron/Jumbotron.js';
+//import Navbar from './Components/Navbar/Navbar.js';
+//import JobForm from "./Components/JobForm/JobForm.js";
+//import { Navbar } from "react-bootstrap";
+import Home from './pages/home.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
-        
+        <Home />
+  
       </header>
     </div>
   );
