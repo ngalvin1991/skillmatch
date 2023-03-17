58a3a378df3ff8a68a06ad244be707f253a7c42a
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-03-17 15:21:49 +0000

    added chakra styling to Field inputs

--- src/Components/JobForm/JobForm.js
+++ src/Components/JobForm/JobForm.js
@@ -1,33 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Formik, Field, Form } from "formik";
-import { Button } from "@chakra-ui/react";
+import { Input, ChakraProvider } from "@chakra-ui/react";
 
 function JobForm() {
-    return (
+  return (
+    <ChakraProvider>
       <div className="form">
         <h1>Search Jobs</h1>
         <h4>Enter up to 5 skills, separated by a comma</h4>
         <h4>Enter your preferred location</h4>
         <Formik
-          initialValues={{ skills: "Skills", city: "Location" }}
+          initialValues={{
+            skills: "Skills",
+            city: "Location"
+          }}
           onSubmit={async (values) => {
             await new Promise((resolve) => setTimeout(resolve, 500));
             alert(JSON.stringify(values, null, 2));
           }}
         >
-  
-          
+
           <Form>
-            <Field name="skills" type="text" />
-            <Field name="city" type="text" />
-            <Button type="submit">Show me my dream job</Button>
+            <Field as={Input}
+              id="skills"
+              name="skills"
+              type="text"
+              variant="filled" />
+            <Field as ={Input} 
+            id="city"
+            name="city" 
+            type="text" 
+            variant="filled"/>
+            <button type="submit">Show me my dream job</button>
           </Form>
         </Formik>
       </div>
-    );
-  }
-  
-  ReactDOM.render(<JobForm />, document.getElementById("root"));
+    </ChakraProvider>
+  );
+}
 
-  export default JobForm;
+ReactDOM.render(<JobForm />, document.getElementById("root"));
+
+export default JobForm;
