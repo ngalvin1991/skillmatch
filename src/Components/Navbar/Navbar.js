cdb6aa888073320039f815c1843d553381117ed3
natalie navickas <natalienavickas@hotmail.com>
2023-03-17 19:53:47 +0000

    navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,45 +1,37 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function Navbar () {
-    return (
-        <ul classname="nav nav-tabs">
-            <li className="nav-item">
-                <NavLink
-                to="/"
-                end
-                className={({isActive}) => 
-                isActive ? 'nav-link active' : 'nav-link'
-            }
-            >
-             Home
-            </NavLink>
-            </li>;
-
-            <li className="nav-item">
-                <NavLink
-                to="jobsearch"
-                className={({ isActive }) =>
-                isActive ? 'nav-link active' : 'nav-link'
-            }
-            >
-            JobSearch
-                </NavLink>
-            </li>
-
-            <li className="nav-item">
-                <NavLink
-                to="contact"
-                className={({ isActive }) =>
-                isActive ? 'nav-link active' : 'nav-link'
-            }
-            >
-            Contact Us 
-                </NavLink>
-            </li>
-        </ul>
-    );
+function Navbars() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="">Home</Nav.Link>
+            <Nav.Link href="">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="">Action</NavDropdown.Item>
+              <NavDropdown.Item href="">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 
-export default Navbar; 
+export default Navbars; 
