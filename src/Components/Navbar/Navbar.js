c5c8f9ee7db1a63eaa74258db86b211ae6aaabae
natalie navickas <natalienavickas@hotmail.com>
2023-03-17 20:23:42 +0000

    logo in navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,19 +4,20 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../Navbar/skillmatchNavbarLogo.jpg';
 
 function Navbars() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} style={{ width: '100px' }}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="">Home</Nav.Link>
-            <Nav.Link href="">Link</Nav.Link>
+            <Nav.Link href="">Job Search</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="">Action</NavDropdown.Item>
+              <NavDropdown.Item href="">Contact Us</NavDropdown.Item>
               <NavDropdown.Item href="">
                 Another action
               </NavDropdown.Item>
