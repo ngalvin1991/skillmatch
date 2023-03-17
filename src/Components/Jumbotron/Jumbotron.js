a94bcb29c30a1ec7e24c93a68e2496fae80af2a6
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-03-17 19:23:10 +0000

    added basic jumbotron layout

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -1,3 +1,16 @@
 import React from "react";
 
+function Jumbotron() {
+    return (
+        <div className="container-fluid bg-dark p-5">
+            
+            <h1 className="display-4">SkillMatch</h1>
+            <p className="lead"></p>
+            <hr className="my-4"></hr>
+                <p></p>
+        </div>
+    )
+}
+
+
 export default Jumbotron; 
