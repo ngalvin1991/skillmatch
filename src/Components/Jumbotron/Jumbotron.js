845332039cb9c63ac135efcb6afb752dfef8ef78
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-03-20 17:59:50 +0000

    removed heading to just leave logo

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -1,14 +1,13 @@
 import React from "react";
 import logo from "../Jumbotron/SkillMatch.gif"
 import "../Jumbotron/Jumbotron.css"
-import { ChakraProvider, Heading, Divider } from "@chakra-ui/react";
-
+import { ChakraProvider,  Divider } from "@chakra-ui/react";
 function Jumbotron() {
     return (
         <ChakraProvider>
         <div className="container-fluid  p-5">
             <img className="center" src={logo}  />
-            <Heading fontSize="7xl" fontFamily="body" className="mb-3" color="purple.500" >SkillMatch</Heading>
+
             <p className="lead">Step into a job that best matches your skills</p>
             <hr className="my-4"></hr>
                 <p>SkillMatch is the perfect way to explore your job and career options. 
