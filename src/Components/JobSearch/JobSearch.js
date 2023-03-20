058e7f1c797bea5e92ddec3f1dcd0072ca8c4418
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-03-20 14:59:59 +0000

    cards centered

--- src/Components/JobSearch/JobSearch.js
+++ src/Components/JobSearch/JobSearch.js
@@ -1,9 +1,12 @@
 import React from "react";
-import getJobAd from "../ApiCall/ApiCall";
+// import getJobAd from "../ApiCall/ApiCall";
+import { ChakraProvider, Center, SimpleGrid, Card, CardHeader, Heading, CardBody, CardFooter, Button, Text } from "@chakra-ui/react";
 
 function JobSearch() {
     return(
-        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+        <ChakraProvider>
+            <Center>
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))' mt="75" mb="75">
   <Card>
     <CardHeader>
       <Heading size='md'> Customer dashboard</Heading>
@@ -25,19 +28,10 @@ function JobSearch() {
     <CardFooter>
       <Button>View here</Button>
     </CardFooter>
-  </Card>
-  <Card>
-    <CardHeader>
-      <Heading size='md'> Customer dashboard</Heading>
-    </CardHeader>
-    <CardBody>
-      <Text>View a summary of all your customers over the last month.</Text>
-    </CardBody>
-    <CardFooter>
-      <Button>View here</Button>
-    </CardFooter>
-  </Card>
+  </Card> 
 </SimpleGrid>
+</Center>
+</ChakraProvider>
     );
 }
 
