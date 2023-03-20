8c1bc848f3843d0c8d9cedbeb18e0d6481867d29
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-03-20 14:26:57 +0000

    added chakra cards

--- src/Components/JobSearch/JobSearch.js
+++ src/Components/JobSearch/JobSearch.js
@@ -1,7 +1,44 @@
 import React from "react";
 import getJobAd from "../ApiCall/ApiCall";
 
+function JobSearch() {
+    return(
+        <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>View here</Button>
+    </CardFooter>
+  </Card>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>View here</Button>
+    </CardFooter>
+  </Card>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>View here</Button>
+    </CardFooter>
+  </Card>
+</SimpleGrid>
+    );
+}
 
-
-
-
+export default JobSearch;
