9f3c05834d8c98d0878ae02bba989b46d0e7eb09
Marvalyn <111514523+Marvalyn@users.noreply.github.com>
2023-03-22 19:20:12 +0000

    modal close button now working

--- src/Components/Modal/modal.js
+++ src/Components/Modal/modal.js
@@ -7,11 +7,13 @@ function ModalComponent(props) {
   console.log(props.company);
   console.log(props.description);
   console.log(props.salaryRange);
-  // function handleHide() {
-  //   setModalShow(false);
-  //   let modalEl = document.getElementById("modal-show");
-  //   modalEl.style.visibility = "hidden"
-  // }
+
+  const [modalShow, setModalShow] = React.useState(false);
+    function handleShow() {
+        setModalShow(false);
+        let modalEl = document.getElementById("modal-show");
+        modalEl.style.visibility = "hidden"
+    }
   return (
     <div
       id="modal-show"
@@ -19,7 +21,7 @@ function ModalComponent(props) {
       style={{ display: 'block', visibility: "hidden" }}
     >
       <Modal.Dialog>
-        <Modal.Header closeButton>
+        <Modal.Header>
           <Modal.Title>{props.title}</Modal.Title>
         </Modal.Header>
 
@@ -30,7 +32,7 @@ function ModalComponent(props) {
         </Modal.Body>
 
         <Modal.Footer>
-          <Button variant="primary">Close</Button>
+          <Button variant="primary" onClick={handleShow} >Close</Button>
         </Modal.Footer>
       </Modal.Dialog>
     </div>
@@ -38,6 +40,4 @@ function ModalComponent(props) {
   );
 }
 
-// { modalShow, setModalShow }, 
-// onClick={handleHide}
 export default ModalComponent;
